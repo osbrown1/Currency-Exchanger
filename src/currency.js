a30682dbe15e83a774418f33250295ea1ad1675a
osbrown1 <socknot@live.com>
2023-02-10 10:38:16 -0800

    starting over to try using fetch instead since wasn't working

--- src/currency.js
+++ src/currency.js
@@ -1,19 +1,2 @@
 export default class Currency {
-  static generateCurrency(generate) {
-    return new Promise(function (resolve, reject) {
-      let request = new XMLHttpRequest();
-      const url = 'https://v6.exchangerate-api.com/v6/c2b3b903e95c1e378e32656c/latest/USD=${process.env.API_KEY}';
-      request.addEventListener("loadend", function() {
-      const response = JSON.parse(this.responseText);
-      console.log('response');
-      if(response.error === false) {
-        resolve(response);
-      } else {
-        reject([this, response]);
-      }
-    });
-    request.open("GET", url, true);
-    request.send()
-  });
-}
-}
+  
--- src/index.js
+++ src/index.js
@@ -6,32 +6,9 @@ import Currency from './currency.js';
 
 //Business Logic
 
-function generateCurrency(generate) {
-  let promise = Currency.generateCurrency(generate);
-  promise.then(function(response) {
-    printElements(response, generate);
-  }, function(errorMessage){
-    printError(errorMessage);
-  });
-}
+
+
 
 
 //UI Logic
 
-function printElements(response) {
-  document.querySelector('#currency-content').innerText = `The currency is: ${response.currency}`;
-}
-
-function printError(error, generate) {
-  document.querySelector('#currency-content').innerText = `There was an error accessing the thing for ${generate}: ${error.message}`;
-}
-
-function handleFormSubmission(event) {
-  event.preventDefault();
-  const generate = document.getElementById('generate-input').value;
-  generateCurrency(generate);
-}
-
-  window.addEventListener("load", function() {
-  document.getElementById("button").addEventListener("click", handleFormSubmission);
-  });
