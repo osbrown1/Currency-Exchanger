350c890d715b902f91bc8dd23c281233556dce6c
osbrown1 <socknot@live.com>
2023-02-10 08:09:15 -0800

    Add tools

--- webpack.config.js
+++ webpack.config.js
@@ -18,7 +18,7 @@ module.exports = {
     new ESLintPlugin(),
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
-      title: 'Shape Tracker',
+      title: 'Currency-Exchanger',
       template: './src/index.html',
       inject: 'body'
     }),
