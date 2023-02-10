deb0b921f73d90bfc1d082e8103419899b283e87
osbrown1 <socknot@live.com>
2023-02-10 14:04:43 -0800

    trying to hide api key in endpoint correctly

--- src/currency.js
+++ src/currency.js
@@ -1,19 +1,4 @@
-export default class Currency { 
-  static generateCurrency(generate) {
-    return new Promise(function (resolve, reject) {
-      let request = new XMLHttpRequest();
-      const url = `https://v6.exchangerate-api.com/v6/c2b3b903e95c1e378e32656c/latest/USD`
-      request.addEventListener("loadend", function () {
-        const response = JSON.parse(this.responseText);
-        console.log(response);
-        if(response.error === false) {
-          resolve(response);
-        } else {
-          reject([this, response]);
-        }
-      });
-      request.open("GET", url, true);
-      request.send();
-    });
-  }
-}
+export default class ExchangeRateService {
+  static getExchangeRate(baseCurrency) {
+  return fetch('https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/${USD}')
+  
