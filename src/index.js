fcf0d15e179f5cb6f15f75dee2166eab676ad3c3
osbrown1 <socknot@live.com>
2023-02-10 10:30:18 -0800

    working on getting webpage to load to test logic

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import 'bootstrap';
-import 'bootstrap/dist/css/botstrap.min.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import Currency from './currency.js';
 
@@ -15,3 +15,23 @@ function generateCurrency(generate) {
   });
 }
 
+
+//UI Logic
+
+function printElements(response) {
+  document.querySelector('#currency-content').innerText = `The currency is: ${response.currency}`;
+}
+
+function printError(error, generate) {
+  document.querySelector('#currency-content').innerText = `There was an error accessing the thing for ${generate}: ${error.message}`;
+}
+
+function handleFormSubmission(event) {
+  event.preventDefault();
+  const generate = document.getElementById('generate-input').value;
+  generateCurrency(generate);
+}
+
+  window.addEventListener("load", function() {
+  document.getElementById("button").addEventListener("click", handleFormSubmission);
+  });
