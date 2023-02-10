83c8a05df4afcfe1466dab8f7303d734c39032cc
osbrown1 <socknot@live.com>
2023-02-10 14:59:31 -0800

    Local Host now works using fetch method, API key is properly tucked in endpoint, async function used now

--- src/currency.js
+++ src/currency.js
@@ -1,4 +1,12 @@
-export default class ExchangeRateService {
-  static getExchangeRate(baseCurrency) {
-  return fetch('https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/${USD}')
-  
+export default class Currency {
+    static getExchangeRate() {
+      return fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`)
+        .then(response => {
+          if (!response.ok) {
+            throw new Error(`${response.status}: ${response.statusText}`);
+          }
+          return response.json();
+        })
+        .catch(error => error);
+    }
+  }
--- src/index.js
+++ src/index.js
@@ -1,10 +1,23 @@
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
-import ExchangeRateService from './currency.js';
+import Currency from './currency.js';
 
 
-// Business Logic
+
+async function handleFormSubmission(event) {
+  event.preventDefault();
+  try {
+    const exchangeRate = await Currency.getExchangeRate();
+    document.querySelector("#showResponse").innerText = `The latest exchange rate for USD is ${exchangeRate.rate}`;
+  } catch (error) {
+    document.querySelector("#showResponse").innerText = `An error occurred: ${error}`;
+  }
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+  document.querySelector("form").addEventListener("submit", handleFormSubmission);
+});
 
 
   
